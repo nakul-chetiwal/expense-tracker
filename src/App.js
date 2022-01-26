9597ad77d07d68880144b56f03cf75fe184c726e
nakul-chetiwal <chetiwalnakul7@gmail.com>
2022-01-26 13:51:51 -0500

    removed comment in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from "./logo.svg";
 import "./App.css";
-//import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 const App = (props) => {
