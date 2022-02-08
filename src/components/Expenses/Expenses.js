3e1e5320d9e325551f16f53d9da13eee845fb75f
nakul-chetiwal <chetiwalnakul7@gmail.com>
2022-02-08 08:13:16 -0500

    conditiona display of list and writing conditional login in separate class

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,29 +3,62 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
-
+import ExpensesList from "./ExpensesList";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  const filterExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  // let expensesContent= <p>No Expenses Found</p>;
+  // if (filterExpenses.length > 0 ){
+  //   expensesContent=filterExpenses.map((expenses) => (
+  //     <ExpenseItem
+  //       key={expenses.id}
+  //       title={expenses.title}
+  //       amount={expenses.amount}
+  //       date={expenses.date}
+  //     />
+  //   ))
+  // }
   return (
+    <li>
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
 
-      {props.expenses.map((expenses) => (
-        <ExpenseItem
-          key={expenses.id}
-          title={expenses.title}
-          amount={expenses.amount}
-          date={expenses.date}
-        />
-      ))}
+      <ExpensesList items={filterExpenses}/>
+      {/* {filterExpenses.length === 0 && <p>No Expenses Found</p>}
+      {filterExpenses.length > 0 &&
+        filterExpenses.map((expenses) => (
+          <ExpenseItem
+            key={expenses.id}
+            title={expenses.title}
+            amount={expenses.amount}
+            date={expenses.date}
+          />
+        ))} */}
+      {/* {filterExpenses.lenght === 0 ? (
+        <p>No Expenses Found</p>
+      ) : (
+        filterExpenses.map((expenses) => (
+          <ExpenseItem
+            key={expenses.id}
+            title={expenses.title}
+            amount={expenses.amount}
+            date={expenses.date}
+          />
+        ))
+      )} */}
+
+      {/* {expensesContent} */}
     </Card>
+    </li>
   );
 }
 export default Expenses;
