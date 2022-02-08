1108b1576e63aa6de0b93fa741d9eb645c9342a9
nakul-chetiwal <chetiwalnakul7@gmail.com>
2022-02-08 11:56:07 -0500

    chartbar added

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,21 @@
 import React from 'react';
-const Chart =props =>{
-
-
-
+import './Chart.css';
+import ChartBar from './ChartBar';
+const Chart =(props) =>{
+  const dataPointValues=props.dataPoints.map((dataPoint) => dataPoint.value);
+  // max function accepts argument list of numbers but not array. therefore spread operator converts array to numbers
+  const totalMaximum=Math.max(...dataPointValues)
+return (
+<div className='chart'>
+ {props.dataPoints.map((dataPoint) => (
+   <ChartBar
+    key={dataPoint.label}
+    value={dataPoint.value}
+    maxValue={totalMaximum}
+    label={dataPoint.label}
+   />
+ ))}
+</div>
+);
 };
 export default Chart;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+//import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
@@ -31,7 +32,7 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-
+      <ExpensesChart expenses={filterExpenses} />
       <ExpensesList items={filterExpenses}/>
       {/* {filterExpenses.length === 0 && <p>No Expenses Found</p>}
       {filterExpenses.length > 0 &&
